9284dde09c82a5521df4727f7807c6508bf619c1
dong4j <code.dsj@gmail.com>
2017-11-19 18:48:26 +0800

    通过 zookeeper 查找服务地址

--- micro-service-lookup/app.js
+++ micro-service-lookup/app.js
@@ -1,13 +1,23 @@
 var express = require('express');
+var zookeeper = require('node-zookeeper-client');
+
+var REGISTRY_ROOT = '/registry';
+var CONNECTION_STRING = '127.0.0.1:2181';
 var PORT = 1234;
+
+// 连接 zookeeper
+var zk = zookeeper.createClient(CONNECTION_STRING);
+zk.connect();
 
 // 启动 web 服务器
 var app = express();
 app.use(express.static('public'));
 app.all('*', function (req, res) {
+    console.log('request path %s', req.path);
     // 处理图标请求
     if (req.path === '/favicon.ico') {
         res.end();
+        return;
     }
 
     // 获取服务名称
@@ -16,7 +26,52 @@ app.all('*', function (req, res) {
     if (!serviceName) {
         console.log('Service-Name request header is not exist');
         res.end();
+        return;
     }
+
+    // 获取服务路径
+    var servicePath = REGISTRY_ROOT + '/' + serviceName;
+    console.log('servicePath: %s', servicePath);
+
+    // 获取服务路径下的节点
+    zk.getChildren(servicePath, function (error, addressNodes) {
+        if (error) {
+            console.log(error.stack);
+            res.end();
+            return;
+        }
+
+        var size = addressNodes.length;
+        if (size === 0) {
+            console.log('address node is not exist');
+            res.end();
+            return;
+        }
+
+        // 生成地址路径
+        var addressPath = servicePath + '/';
+        if (size === 1) {
+            addressPath += addressNodes[0];
+        } else {
+            // 若存在多个地址,随机选取一个
+            addressPath += addressNodes[parseInt(Math.random() * size)];
+        }
+        console.log('addressPath: %s', addressPath);
+
+        // 获取服务地址
+        zk.getData(addressPath, function (error, serviceAddress) {
+            if (error) {
+                console.log(error.stack);
+                res.end();
+                return;
+            }
+            console.log('serviceAddress: %s', serviceAddress);
+            if (!serviceAddress) {
+                console.log('service address  is not exist');
+                res.end();
+            }
+        });
+    })
 });
 
 app.listen(PORT, function () {
