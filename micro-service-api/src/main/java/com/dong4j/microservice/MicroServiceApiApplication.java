6047e5f05058c4b0101752c7e5915e2cffa44044
dong4j <code.dsj@gmail.com>
2017-11-19 15:38:26 +0800

    init

---
+++ micro-service-api/src/main/java/com/dong4j/microservice/MicroServiceApiApplication.java
@@ -0,0 +1,19 @@
+package com.dong4j.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author dong4j.
+ * email dong4j@gmail.com
+ * date 2017年10月24日 上午10点:34分
+ * describe  spring boot 启动类
+ */
+@SpringBootApplication
+public class MicroServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceApiApplication.class, args);
+	}
+}
